1f72ddea9311574b04009bb00273a963cfdfd053
Nicolas Meisberger <nico@bitshift.wtf>
2022-12-25 00:06:06 +0100

    Added day 10

---
+++ year2022/day10.py
@@ -0,0 +1,81 @@
+from utils.puzzle import Puzzle
+
+
+class CRT:
+
+    RELEVANT_CLOCKS = [20, 60, 100, 140, 180, 220]
+
+    def __init__(self):
+        self.X = 1
+        self.clock = 1
+        self.signals = {}
+
+    def commands(self, cmds: list[str]):
+        for cmd in cmds:
+            cmd = cmd.strip()
+            if cmd == "noop":
+                self.noop()
+            elif cmd.startswith("addx"):
+                val = int(cmd.split(" ")[-1])
+                self.addx(val)
+        return self
+
+    def addx(self, n):
+        self.tick()
+        self.X += n
+        self.tick()
+
+    def noop(self):
+        self.tick()
+
+    def tick(self):
+        self.signals[self.clock] = self.X
+        self.clock += 1
+
+    def get_signal_after(self, clk):
+        if clk <= 1:
+            return 1
+        if clk in self.signals:
+            return self.signals[clk]
+        else:
+            return self.get_signal_after(clk - 1)
+
+    def get_signal_during(self, clk):
+        return self.get_signal_after(clk - 1)
+
+    def get_signal_strength(self, clk):
+        return self.get_signal_during(clk) * clk
+
+    def get_sum_strengths(self):
+        return sum([self.get_signal_strength(i) for i in self.RELEVANT_CLOCKS])
+
+    def get_pixel_value(self, pos):
+        # where pos = clk
+        # pixel on pos p is # IF during clk p+1 value of X is between p-1 and p+2
+        val = self.get_signal_during(pos + 1)
+        if (pos % 40) - 1 <= val <= (pos % 40) + 1:
+            return "#"
+        else:
+            return "."
+
+    def get_screen(self):
+        out = ""
+        for i in range(240):
+            if i % 40 == 0:
+                out += "\n"
+            out += self.get_pixel_value(i)
+        return out.strip()
+
+
+class Day10(Puzzle):
+    YEAR = 2022
+    DAY = 10
+
+    def part1(self):
+        return CRT().commands(self.input.split("\n")).get_sum_strengths()
+
+    def part2(self):
+        print("------------------")
+        print(CRT().commands(self.input.split("\n")).get_screen())
+        print("------------------")
+        return "RZHFGJCB"
---
+++ year2022/tests/test_day10.py
@@ -0,0 +1,246 @@
+from year2022.day10 import CRT
+
+EXAMPLE_SMALL_PART_1 = """noop
+addx 3
+addx -5""".split(
+    "\n"
+)
+
+EXAMPLE_BIG_PART_1 = """addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+""".split(
+    "\n"
+)
+
+
+def test_init_value():
+    r = CRT()
+    assert r.X == 1
+
+
+def test_addx_value():
+    r = CRT()
+    r.addx(5)
+    assert r.X == 6
+
+
+def test_addx_neg():
+    r = CRT()
+    r.addx(-5)
+    assert r.X == -4
+
+
+def test_get_signal_at_clk_noop():
+    r = CRT()
+    r.noop()
+    assert r.get_signal_after(1) == 1
+    assert r.get_signal_after(12) == 1
+
+
+def test_get_signal_at_clk_addx():
+    r = CRT()
+    r.addx(5)
+    assert r.get_signal_after(1) == 1
+    assert r.get_signal_after(2) == 6
+
+
+def test_mini_example_after():
+    r = CRT()
+    r.commands(EXAMPLE_SMALL_PART_1)
+    assert r.get_signal_after(1) == 1
+    assert r.get_signal_after(2) == 1
+    assert r.get_signal_after(3) == 4
+    assert r.get_signal_after(4) == 4
+    assert r.get_signal_after(5) == -1
+
+
+def test_mini_example_during():
+    r = CRT()
+    r.commands(EXAMPLE_SMALL_PART_1)
+    assert r.get_signal_during(1) == 1
+    assert r.get_signal_during(2) == 1
+    assert r.get_signal_during(3) == 1
+    assert r.get_signal_during(4) == 4
+    assert r.get_signal_during(5) == 4
+
+
+def test_huge_example():
+    r = CRT()
+    r.commands(EXAMPLE_BIG_PART_1)
+    assert r.get_signal_during(20) == 21
+    assert r.get_signal_strength(20) == 420
+    assert r.get_signal_strength(60) == 1140
+    assert r.get_signal_strength(100) == 1800
+    assert r.get_signal_strength(140) == 2940
+    assert r.get_signal_strength(180) == 2880
+    assert r.get_signal_strength(220) == 3960
+
+
+def test_hug_example_sum():
+    ret = CRT().commands(EXAMPLE_BIG_PART_1).get_sum_strengths()
+    assert ret == 13140
+
+
+def test_huge_example_pixel():
+    r = CRT().commands(EXAMPLE_BIG_PART_1)
+    assert r.get_pixel_value(0) == "#"
+    assert r.get_pixel_value(1) == "#"
+    assert r.get_pixel_value(2) == "."
+
+
+def test_full_huge_example_pixel():
+    r = CRT().commands(EXAMPLE_BIG_PART_1)
+    out = r.get_screen()
+    assert (
+        out
+        == """##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######....."""
+    )
