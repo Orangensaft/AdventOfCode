ccc9cfd01b03e6562473f816122cf44546fa30e8
Nicolas Meisberger <nicolas.meisberger@fintiba.com>
2023-12-05 14:51:39 +0100

    Add a super unoptimized solution for day5

---
+++ year2023/day5.py
@@ -0,0 +1,115 @@
+from concurrent.futures import ThreadPoolExecutor
+from functools import lru_cache
+
+from utils.puzzle import Puzzle
+
+
+class SeedMap:
+    def __init__(self, input_str: str):
+        self.input_str = input_str
+        seeds, *maps = self.get_input_parts()
+        self.seeds = [int(s) for s in seeds.split(": ")[-1].split(" ")]
+        self.seed_ranges: [range] = self.create_seed_ranges()
+        self.maps = self.process_maps(maps)
+
+    def get_input_parts(self):
+        parts = self.input_str.split("\n\n")
+        return parts
+
+    @staticmethod
+    def process_map(map_str):
+        out = {}
+        lines = map_str.split("\n")
+        out["name"] = lines.pop(0).replace(" map:", "")
+        for line in lines:
+            destination_range_start, source_range_start, range_length = [
+                int(i) for i in line.split(" ")
+            ]
+            out[
+                range(source_range_start, source_range_start + range_length)
+            ] = destination_range_start
+        return out
+
+    def process_maps(self, maps: [str]):
+        out = []
+        for map_str in maps:
+            out.append(self.process_map(map_str))
+        return out
+
+    @lru_cache(maxsize=None)
+    def get_location(self, object_id: int, lookup_id: int) -> int:
+        lookup = self.maps[lookup_id]
+        # example: find soil from seed
+        for start_range, destination_start in lookup.items():
+            if start_range == "name":
+                continue
+            if object_id in start_range:
+                offset = object_id - start_range.start
+                return destination_start + offset
+        return object_id
+
+    def get_end_position(self, object_id: int) -> int:
+        cur = object_id
+        for mapping in range(len(self.maps)):
+            cur = self.get_location(cur, mapping)
+        return cur
+
+
+    def get_all_end_positions(self):
+        out = []
+        for seed in self.seeds:
+            out.append(self.get_end_position(seed))
+        return out
+
+    def get_lowest_position(self):
+        return min(self.get_all_end_positions())
+
+    def create_seed_ranges(self):
+        out = []
+        for i in range(len(self.seeds)//2):
+            start, count = self.seeds[i*2], self.seeds[(i*2)+1]
+            out.append(range(start, start+count))
+        return out
+
+    def get_total_range_size(self):
+        total = 0
+        for r in self.seed_ranges:
+            total += (r.stop - r.start)
+        return total
+
+    def get_lowest_for_seed_range(self, r: range):
+        lowest = 999999999999999
+        with ThreadPoolExecutor(max_workers=8) as exe:
+            results = list(exe.map(self.get_end_position, r))
+        """
+        for i in r:
+            end_location = self.get_end_position(i)
+            if end_location < lowest:
+                lowest = end_location
+        
+        return lowest
+        """
+        return min(results)
+
+    def get_lowest_part2_position(self):
+        lowest = 999999999999999999999999999
+        total_seeds = self.get_total_range_size()
+        print(f"Total seeds: {total_seeds}")
+        print(f"Seed ranges: {len(self.seed_ranges)}")
+        for i, seed_range in enumerate(self.seed_ranges):
+            print(f"{i}/{len(self.seed_ranges)}")
+            l = self.get_lowest_for_seed_range(seed_range)
+            print(f"Seed range {i}: Lowest: {l}")
+            if l < lowest:
+                lowest = l
+        return lowest
+
+class AintMuchButHonestWork(Puzzle):
+    DAY = 5
+    YEAR = 2023
+
+    def part1(self):
+        return SeedMap(self.input).get_lowest_position()
+
+    def part2(self):
+        return SeedMap(self.input).get_lowest_part2_position()
---
+++ year2023/tests/test_day5.py
@@ -0,0 +1,107 @@
+from year2023.day5 import SeedMap
+
+EXAMPLE = """seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4"""
+
+
+def test_get_map_parts():
+    m = SeedMap(EXAMPLE)
+    parts = m.get_input_parts()
+    assert len(parts) == 8
+
+
+def test_get_seed_list():
+    m = SeedMap(EXAMPLE)
+    assert m.seeds == [79, 14, 55, 13]
+
+
+def test_process_map_name():
+    m = SeedMap(EXAMPLE)
+    assert m.maps[0]["name"] == "seed-to-soil"
+    assert m.maps[1]["name"] == "soil-to-fertilizer"
+    assert m.maps[2]["name"] == "fertilizer-to-water"
+    assert m.maps[3]["name"] == "water-to-light"
+    assert m.maps[4]["name"] == "light-to-temperature"
+    assert m.maps[5]["name"] == "temperature-to-humidity"
+    assert m.maps[6]["name"] == "humidity-to-location"
+    assert len(m.maps) == 7
+
+
+def test_get_location():
+    m = SeedMap(EXAMPLE)
+    seed_to_soil = m.maps[0]
+    assert seed_to_soil["name"] == "seed-to-soil"
+    assert m.get_location(96, 0) == 98
+
+
+def test_get_soil_numbers():
+    m = SeedMap(EXAMPLE)
+    out = []
+    for seed in m.seeds:
+        out.append(m.get_location(seed, 0))
+    assert out == [81, 14, 57, 13]
+
+
+def test_get_end_position_single():
+    m = SeedMap(EXAMPLE)
+    assert m.get_end_position(79) == 82
+
+
+def test_get_all_end_positions():
+    m = SeedMap(EXAMPLE)
+    all_positions = m.get_all_end_positions()
+    assert all_positions == [82, 43, 86, 35]
+
+
+def test_get_lowest_location():
+    m = SeedMap(EXAMPLE)
+    lowest = m.get_lowest_position()
+    assert lowest == 35
+
+
+def test_get_seed_ranges():
+    m = SeedMap(EXAMPLE)
+    ranges = m.seed_ranges
+    assert len(ranges) == 2
+    range1, range2 = ranges
+    assert range1.start == 79
+    assert range1.stop == 79+14
+    assert range2.start == 55
+    assert range2.stop == 55+13
+
+
+def test_lowest_part_2():
+    m = SeedMap(EXAMPLE)
+    r = m.get_lowest_part2_position()
+    assert r == 46
